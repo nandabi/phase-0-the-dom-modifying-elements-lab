7cf94fddaef345030869dbfb30f7029a0137d48a
nandabi <valerie.nandabi@student.moringaschool.com>
2024-01-09 12:09:38 +0300

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const testVar = "h1"
+document.querySelector("main#main").remove ()
+const newHeader =
+document.createElement("h1")
+
+   newHeader.id = "victory"
+   newHeader.innerHTML = "Meyer is the champion!"
+   newHeader.className = "victory"
